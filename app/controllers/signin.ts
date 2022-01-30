8236a251fad867465dcc23364ca38aae9578e7db
Victor Shermon <victor804.gt@gmail.com>
2022-01-30 10:10:46 -0300

    feat: create page dashboard

--- app/controllers/signin.ts
+++ app/controllers/signin.ts
@@ -4,13 +4,21 @@ import { MysqlConnect } from '../helpers/database'
 import { User } from '../entity/User'
 
 export const home = (req: Request, res: Response) => {
-  res.render('pages/signin.ejs')
+  res.render('pages/signin.ejs', { error: '', name: '' })
 }
 
 export const signin = async (req: Request, res: Response) => {
   const user = MysqlConnect.getRepository(User)
   const info = await user.find({ email: req.body.email })
-  res.send(info)
+  if (info[0]) {
+    console.log(info[0])
+    const password = await bcrypt.compare(req.body.password, info[0].password)
+    if (!password) {
+      res.render('pages/signin.ejs', { error: 'senha errada!', name: '' })
+    }
+    res.render('pages/dashboard.ejs', { name: info[0].name })
+  }
+  res.render('pages/signin.ejs', { error: 'Email não cadastrado!', name: '' })
 }
 
 export default {
